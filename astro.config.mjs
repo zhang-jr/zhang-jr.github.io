78dcd6ccad12031889e9265a60b868ad01708f29
zhang-jr <jrzhang339@gmail.com>
2026-03-17 11:06:16 +0800

    fix: remove sitemap integration to fix CI build crash

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://zhang-jr.github.io',
   output: 'static',
-  integrations: [sitemap()],
 });
